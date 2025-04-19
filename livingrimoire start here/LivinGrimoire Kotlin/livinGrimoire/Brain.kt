b2a549feb8ed13fdecdd576a57d9373d16ea335d
moti barski <yottaenkigo@yahoo.com>
2025-04-19 09:28:24 +0300

    Brain upgrade java KT 190425

--- livingrimoire start here/LivinGrimoire Kotlin/livinGrimoire/Brain.kt
+++ livingrimoire start here/LivinGrimoire Kotlin/livinGrimoire/Brain.kt
@@ -1,135 +1,74 @@
 package livinGrimoire
 
-/*********
- * intro *
- *
- *
- * up until now, the LivinGrimoire was on par with the matrix learn scene.
- * one line of code to add one skill.
- *
- * that is great, that is sci-fi turned real, that is the most significant coding achievement in the history of time.
- *
- * but hey why stop there? why only be on par with the matrix and the human brain ?
- * what is beyond the matrix level? you already know
- *
- * cyberpunk>the matrix.
- * one line of code to add a skill, but ALSO! 1 line of code to add a hardware capability.
- *
- *
- * Atributes*
- *
- *
- * the logicChobit is a Chobits attribute with logic skills. these skills have algorithmic logic,
- * and thinking patterns.
- *
- * the hardwareChobit is a Chobit attribute with hardware skills. these skills access the
- * hardware capabilities of the machine.
- * for example: output printing, sending mail, sending SMS, making a phone call, taking
- * a photo, accessing GPIO pins, opening a program, fetching the weather and so on.
- *
- *
- * special attributes*
- *
- *
- * in some cases the hardware chobit may want to send a message to the logic chobit,
- * for example to give feedback on hardware components. this is handled by the bodyInfo
- * String.
- *
- * the emot attribute is the chobit's current emotion.
- *
- * the logicChobitOutput is the chobit's last output.
- *
- *
- * hardware skill types*
- *
- *
- * assembly style: these skills are triggered by strings with certain wild card characters
- * for example: #open browser
- *
- * funnel: these are triggered by strings without wild cards.
- * for example: "hello world"->prints hello world
- *
- *
- * example use*
- *
- * DiSysOut is an example of a hardware skill
- *
- * see Brain main for example use of the cyberpunk Software Design Pattern
- */
 class Brain {
     var logicChobit = Chobits()
-    private var emotion = ""
-    private var bodyInfo = ""
-    private var logicChobitOutput = ""
+
+    // ret active alg part representing emotion
+    var emotion = ""
+        private set
+
+    // ret feedback (last output)
+    var logicChobitOutput = ""
+        private set
     var hardwareChobit = Chobits()
     var ear = Chobits() // 120425 upgrade
     var skin = Chobits()
     var eye = Chobits()
-    fun getEmotion(): String {
-        return emotion
-    }
 
-    fun getBodyInfo(): String {
-        return bodyInfo
-    }
-
-    fun getLogicChobitOutput(): String {
-        return logicChobitOutput
-    }
-
+    //c'tor
     init {
         imprintSoul(logicChobit.kokoro, hardwareChobit, ear, skin, eye)
     }
 
+    // live
     fun doIt(ear: String, skin: String, eye: String) {
-        logicChobitOutput = if (bodyInfo.isNotEmpty()) {
-            logicChobit.think(ear, bodyInfo, eye)
-        } else {
-            logicChobit.think(ear, skin, eye)
-        }
+        logicChobitOutput = logicChobit.think(ear, skin, eye)
         emotion = logicChobit.soulEmotion
-        // case: hardware skill wishes to pass info to logical chobit
-        bodyInfo = hardwareChobit.think(logicChobitOutput, skin, eye)
+        hardwareChobit.think(logicChobitOutput, skin, eye)
     }
 
+    // add regular thinking(logical) skill
     fun addLogicalSkill(skill: Skill) {
         logicChobit.addSkill(skill)
     }
 
+    // add output skill
     fun addHardwareSkill(skill: Skill) {
         hardwareChobit.addSkill(skill)
     }
 
-    // 120425 upgrade
+    // add audio(ear) input skill
     fun addEarSkill(skill: Skill) {
         ear.addSkill(skill)
     }
 
+    // add sensor input skill
     fun addSkinSkill(skill: Skill) {
         skin.addSkill(skill)
     }
 
+    // add visual input skill
     fun addEyeSkill(skill: Skill) {
         eye.addSkill(skill)
     }
 
-    fun think(ear: String) {
-        if (ear.isNotEmpty()) {
-            // handles typed inputs
-            doIt(ear, "", "")
+    fun think(keyIn: String) {
+        if (!keyIn.isEmpty()) {
+            // handles typed inputs(keyIn)
+            doIt(keyIn, "", "")
         } else {
             // accounts for sensory inputs
-            doIt(this.ear.think("", "", ""), skin.think("", "", ""), eye.think("", "", ""))
+            doIt(ear.think("", "", ""), skin.think("", "", ""), eye.think("", "", ""))
         }
     }
 
     fun think() {
-        // accounts for sensory inputs
+        // accounts for sensory inputs only. use this overload for tick events(where it is certain no typed inputs are to be processed)
         doIt(ear.think("", "", ""), skin.think("", "", ""), eye.think("", "", ""))
     }
 
     companion object {
-        fun imprintSoul(kokoro: Kokoro, vararg args: Chobits) {
+        private fun imprintSoul(kokoro: Kokoro, vararg args: Chobits) {
             for (arg in args) {
                 arg.kokoro = kokoro
             }
--- livingrimoire start here/LivinGrimoire java/LivinGrimoire/Brain.java
+++ livingrimoire start here/LivinGrimoire java/LivinGrimoire/Brain.java
@@ -1,127 +1,63 @@
 package LivinGrimoire;
-/*********
- *intro *
- ********
-
- up until now, the LivinGrimoire was on par with the matrix learn scene.
- one line of code to add one skill.
-
- that is great, that is sci-fi turned real, that is the most significant coding achievement in the history of time.
-
- but hey why stop there? why only be on par with the matrix and the human brain ?
- what is beyond the matrix level? you already know
-
- cyberpunk>the matrix.
- one line of code to add a skill, but ALSO! 1 line of code to add a hardware capability.
-
- ***********
- *Atributes*
- ***********
-
- the logicChobit is a Chobits attribute with logic skills. these skills have algorithmic logic,
- and thinking patterns.
-
- the hardwareChobit is a Chobit attribute with hardware skills. these skills access the
- hardware capabilities of the machine.
- for example: output printing, sending mail, sending SMS, making a phone call, taking
- a photo, accessing GPIO pins, opening a program, fetching the weather and so on.
-
- ********************
- *special attributes*
- ********************
-
- in some cases the hardware chobit may want to send a message to the logic chobit,
- for example to give feedback on hardware components. this is handled by the bodyInfo
- String.
-
- the emot attribute is the chobit's current emotion.
-
- the logicChobitOutput is the chobit's last output.
-
- **********************
- *hardware skill types*
- **********************
-
- assembly style: these skills are triggered by strings with certain wild card characters
- for example: #open browser
-
- funnel: these are triggered by strings without wild cards.
- for example: "hello world"->prints hello world
-
- *************
- *example use*
- *************
- DiSysOut is an example of a hardware skill
-
- see Brain main for example use of the cyberpunk Software Design Pattern
- */
 public class Brain {
     public Chobits logicChobit = new Chobits();
     private String emotion = "";
-    private String bodyInfo = "";
     private String logicChobitOutput = "";
     public Chobits hardwareChobit = new Chobits();
     public Chobits ear = new Chobits(); // 120425 upgrade
     public Chobits skin = new Chobits();
     public Chobits eye = new Chobits();
-
+    // ret active alg part representing emotion
     public String getEmotion() {
         return emotion;
     }
-
-    public String getBodyInfo() {
-        return bodyInfo;
-    }
-
+    // ret feedback (last output)
     public String getLogicChobitOutput() {
         return logicChobitOutput;
     }
-
+    //c'tor
     public Brain() {
         Brain.imprintSoul(this.logicChobit.getKokoro(), this.hardwareChobit, this.ear, this.skin, this.eye);
     }
-    public static void imprintSoul(Kokoro kokoro, Chobits... args) {
+    private static void imprintSoul(Kokoro kokoro, Chobits... args) {
         for (Chobits arg : args) {
             arg.setKokoro(kokoro);
         }
     }
+    // live
     public void doIt(String ear, String skin, String eye) {
-        if (!bodyInfo.isEmpty()){
-            logicChobitOutput = logicChobit.think(ear,bodyInfo,eye);
-        }
-        else{
-            logicChobitOutput = logicChobit.think(ear,skin,eye);
-        }
+        logicChobitOutput = logicChobit.think(ear,skin,eye);
         emotion = logicChobit.getSoulEmotion();
-        // case: hardware skill wishes to pass info to logical chobit
-        bodyInfo = hardwareChobit.think(logicChobitOutput,skin,eye);
+        hardwareChobit.think(logicChobitOutput,skin,eye);
     }
+    // add regular thinking(logical) skill
     public void addLogicalSkill(Skill skill){logicChobit.addSkill(skill);}
+    // add output skill
     public void addHardwareSkill(Skill skill){hardwareChobit.addSkill(skill);}
-    // 120425 upgrade
+    // add audio(ear) input skill
     public void addEarSkill(Skill skill) {
         this.ear.addSkill(skill);
     }
-
+    // add sensor input skill
     public void addSkinSkill(Skill skill) {
         this.skin.addSkill(skill);
     }
-
+    // add visual input skill
     public void addEyeSkill(Skill skill) {
         this.eye.addSkill(skill);
     }
-    public void think(String ear) {
-        if (!ear.isEmpty()) {
-            // handles typed inputs
-            this.doIt(ear, "", "");
+    public void think(String keyIn) {
+        if (!keyIn.isEmpty()) {
+            // handles typed inputs(keyIn)
+            this.doIt(keyIn, "", "");
         } else {
             // accounts for sensory inputs
-            this.doIt(this.ear.think("", "", ""), this.skin.think("", "", ""), this.eye.think("", "", ""));
+            this.doIt(ear.think("", "", ""), skin.think("", "", ""), eye.think("", "", ""));
         }
     }
 
     public void think() {
-        // accounts for sensory inputs
-        this.doIt(this.ear.think("", "", ""), this.skin.think("", "", ""), this.eye.think("", "", ""));
+        // accounts for sensory inputs only. use this overload for tick events(where it is certain no typed inputs are to be processed)
+        this.doIt(ear.think("", "", ""), skin.think("", "", ""), eye.think("", "", ""));
     }
 }
