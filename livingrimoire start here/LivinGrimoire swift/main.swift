011c945323b4976ced3627178cf2694b1f5d58d7
moti barski <yottaenkigo@yahoo.com>
2025-07-28 00:39:54 +0300

    swift main async input loop 280725

--- livingrimoire start here/LivinGrimoire swift/main.swift
+++ livingrimoire start here/LivinGrimoire swift/main.swift
@@ -8,7 +8,39 @@ import Foundation
 
 
 
-let b1:Brain = Brain()
-b1.addSkill(DiHelloWorld())
-b1.addSkill(DiSysOut())
-b1.think("hello")
+let brain = Brain()
+    .chained(DiHelloWorld())
+    .chained(DiTime())
+    .chained(DiSysOut())
+
+let brainQueue = DispatchQueue(label: "com.livingrimoire.queue")
+let tickInterval: TimeInterval = 2
+
+func brainLoop() {
+    while true {
+        let message = readLine() ?? ""
+        brain.think(message)
+
+        if message.lowercased() == "exit" {
+            print("Exiting...")
+            exit(0)
+        }
+    }
+}
+
+func tickLoop() {
+    var nextTick = Date().timeIntervalSince1970
+    while true {
+        let now = Date().timeIntervalSince1970
+        if now >= nextTick {
+            brainQueue.async {
+                brain.think()
+            }
+            nextTick += tickInterval
+        }
+        Thread.sleep(forTimeInterval: 0.01)
+    }
+}
+
+DispatchQueue.global().async { tickLoop() }
+brainLoop()
