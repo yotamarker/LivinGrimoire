f7d8aaad97a9f8c0ec8b46d419b82ce5f839804a
moti barski <yottaenkigo@yahoo.com>
2025-09-23 07:27:11 +0300

    java RefreshQ bias draw 230925
    java RefreshQ bias draw methods added
    to favor recent or old data

--- livingrimoire start here/LivinGrimoire java/Auxiliary_Modules/RefreshQ.java
+++ livingrimoire start here/LivinGrimoire java/Auxiliary_Modules/RefreshQ.java
@@ -1,5 +1,7 @@
 package Auxiliary_Modules;
 
+import java.util.Random;
+
 public class RefreshQ extends UniqueItemSizeLimitedPriorityQueue{
     public void removeItem(String item){
         super.elements.remove(item);
@@ -19,6 +21,50 @@ public class RefreshQ extends UniqueItemSizeLimitedPriorityQueue{
             poll();
         }
         elements.add(data);
+    }
+    public String pickRecentWeighted() {
+        int size = this.elements.size();
+        if (size == 0) return null;
+
+        int totalWeight = 0;
+        int[] weights = new int[size];
+        for (int i = 0; i < size; i++) {
+            weights[i] = i + 1;
+            totalWeight += weights[i];
+        }
+
+        int rnd = new Random().nextInt(totalWeight);
+        int cumulative = 0;
+        for (int i = 0; i < size; i++) {
+            cumulative += weights[i];
+            if (rnd < cumulative) {
+                return this.elements.get(i);
+            }
+        }
+
+        return this.elements.get(size - 1);
+    }
+    public String pickOldWeighted() {
+        int size = this.elements.size();
+        if (size == 0) return null;
+
+        int totalWeight = 0;
+        int[] weights = new int[size];
+        for (int i = 0; i < size; i++) {
+            weights[i] = size - i; // Oldest = size, Newest = 1
+            totalWeight += weights[i];
+        }
+
+        int rnd = new Random().nextInt(totalWeight);
+        int cumulative = 0;
+        for (int i = 0; i < size; i++) {
+            cumulative += weights[i];
+            if (rnd < cumulative) {
+                return this.elements.get(i);
+            }
+        }
+
+        return this.elements.get(0); // fallback to oldest
     }
 
 }
