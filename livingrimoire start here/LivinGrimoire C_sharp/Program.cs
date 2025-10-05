2d9b2f9496ae1cea4a838429c6fa6f0093b8deeb
moti barski <yottaenkigo@yahoo.com>
2025-10-05 20:13:00 +0300

    dot net sparks of life 051025

--- livingrimoire start here/LivinGrimoire C_sharp/Program.cs
+++ livingrimoire start here/LivinGrimoire C_sharp/Program.cs
@@ -1,5 +1,49 @@
-﻿Brain brain = new Brain();
-Personality.LoadSkills(brain);
-brain.Think("hello");
-brain.Think("what is the time");
-Console.ReadLine();
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static Brain brain = new Brain();
+    static int tickInterval = 2000; // milliseconds
+
+    static void Main(string[] args)
+    {
+        Personality.LoadSkills(brain);
+
+        // Start tick loop in background
+        Task.Run(() => TickLoop());
+
+        // Start input loop
+        BrainLoop();
+    }
+
+    static void BrainLoop()
+    {
+        while (true)
+        {
+            string message = Console.ReadLine() ?? "";
+            brain.Think(message);
+            if (message.ToLower() == "exit")
+            {
+                Console.WriteLine("Exiting...");
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    static void TickLoop()
+    {
+        DateTime nextTick = DateTime.Now;
+        while (true)
+        {
+            if (DateTime.Now >= nextTick)
+            {
+                Task.Run(() => brain.Think());
+                nextTick = nextTick.AddMilliseconds(tickInterval);
+            }
+            Thread.Sleep(10);
+        }
+    }
+}
--- livingrimoire start here/LivinGrimoire VB.net/Program.vb
+++ livingrimoire start here/LivinGrimoire VB.net/Program.vb
@@ -1,12 +1,40 @@
 Imports System
+Imports System.Threading
+Imports System.Threading.Tasks
 
 Module Program
-    Sub Main(args As String())
-        Dim brain As New Brain()
+    Dim brain As New Brain()
+    Dim tickInterval As Integer = 2000 ' milliseconds
+
+    Sub Main()
         Personality.Personality.Load(brain)
-        brain.think("hello")
-        brain.think("what is the time")
-        brain.Think()
-        Console.ReadLine()
+        ' Start tick loop in background
+        Task.Run(Sub() TickLoop())
+
+        ' Start input loop
+        BrainLoop()
+    End Sub
+
+    Sub BrainLoop()
+        While True
+            Dim message As String = Console.ReadLine()
+            If message Is Nothing Then message = ""
+            brain.think(message)
+            If message.ToLower() = "exit" Then
+                Console.WriteLine("Exiting...")
+                Environment.Exit(0)
+            End If
+        End While
+    End Sub
+
+    Sub TickLoop()
+        Dim nextTick As DateTime = DateTime.Now
+        While True
+            If DateTime.Now >= nextTick Then
+                Task.Run(Sub() brain.think())
+                nextTick = nextTick.AddMilliseconds(tickInterval)
+            End If
+            Thread.Sleep(10)
+        End While
     End Sub
 End Module
