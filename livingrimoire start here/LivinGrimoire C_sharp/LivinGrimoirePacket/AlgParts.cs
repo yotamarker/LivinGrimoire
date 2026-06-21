dbca9b6590dc995b240a4891b38205e6c6dfecd0
yotamarker <yotamarker@protonmail.ch>
2026-06-21 18:11:23 +0300

    algparts update csharp 210626

--- livingrimoire start here/LivinGrimoire C_sharp/LivinGrimoirePacket/AlgParts.cs
+++ livingrimoire start here/LivinGrimoire C_sharp/LivinGrimoirePacket/AlgParts.cs
@@ -1,184 +1,264 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-public class APSleep : AlgPart
+namespace LivinGrimoirePacket.AlgParts
 {
-    private Responder wakeners;
-    private bool done = false;
-    private TimeGate timeGate;
-
-    public APSleep(Responder wakeners, int sleepMinutes)
+    public class APSleep : AlgPart
     {
-        this.wakeners = wakeners;
-        this.timeGate = new TimeGate(sleepMinutes);
-        this.timeGate.OpenGate();
-    }
+        private readonly Responder wakeners;
+        private bool done = false;
+        private readonly TimeGate timeGate;
 
-    public override string Action(string ear, string skin, string eye)
-    {
-        if (wakeners.ResponsesContainsStr(ear) || timeGate.IsClosed())
+        public APSleep(Responder wakeners, int sleepMinutes)
         {
-            done = true;
-            return "i am awake";
+            this.wakeners = wakeners;
+            timeGate = new TimeGate(sleepMinutes);
+            timeGate.OpenGate();
         }
-        if (!string.IsNullOrEmpty(ear))
+
+        public override string Action(string ear, string skin, string eye)
         {
-            return "zzz";
-        }
-        return "";
-    }
-
-    public override bool Completed()
-    {
-        return done;
-    }
-}
-
-public class APsay : AlgPart
-{
-    private int at = 10;
-    private string param = "hmm";
-
-    public APsay() { }
-
-    public APsay(int repetitions, string param)
-    {
-        if (repetitions < at) { this.at = repetitions; }
-        this.param = param;
-    }
-
-    public override string Action(string ear, string skin, string eye)
-    {
-        string axnStr = "";
-        if (this.at > 0)
-        {
-            if (ear.ToLower() != this.param)
+            if (wakeners.ResponsesContainsStr(ear) || timeGate.IsClosed())
             {
-                axnStr = this.param;
-                this.at -= 1;
+                done = true;
+                return "i am awake";
             }
+            if (!string.IsNullOrEmpty(ear))
+                return "zzz";
+            return "";
         }
-        return axnStr;
+
+        public override bool Completed() => done;
     }
 
-    public override bool Completed()
+    public class APSay : AlgPart
     {
-        return this.at < 1;
-    }
-}
+        private int at;
+        private readonly string param;
 
-public class APMad : AlgPart
-{
-    private readonly Queue<string> _sentences;
+        public APSay(int at, string param)
+        {
+            this.at = at > 10 ? 10 : at;
+            this.param = param;
+        }
 
-    // Constructor for params
-    public APMad(params string[] sentences)
-    {
-        _sentences = new Queue<string>(sentences);
-    }
+        public override string Action(string ear, string skin, string eye)
+        {
+            string axnStr = "";
+            if (at > 0)
+            {
+                if (!string.Equals(ear, param, StringComparison.OrdinalIgnoreCase))
+                {
+                    axnStr = param;
+                    at -= 1;
+                }
+            }
+            return axnStr;
+        }
 
-    // Constructor for List
-    public APMad(List<string> list1)
-    {
-        _sentences = new Queue<string>(list1);
-    }
-
-    // O(1) dequeue
-    public override string Action(string ear, string skin, string eye)
-    {
-        return _sentences.Count > 0 ? _sentences.Dequeue() : "";
-    }
-
-    // O(1) completed check
-    public override bool Completed()
-    {
-        return _sentences.Count == 0;
-    }
-}
-
-public class APShy : AlgPart
-{
-    private readonly Queue<string> _sentences;
-
-    // Constructor for params
-    public APShy(params string[] sentences)
-    {
-        _sentences = new Queue<string>(sentences);
+        public override bool Completed() => at < 1;
     }
 
-    // Constructor for List
-    public APShy(List<string> list1)
+    public class APMad : AlgPart
     {
-        _sentences = new Queue<string>(list1);
+        private readonly Queue<string> sentences;
+
+        public APMad(params string[] sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
+
+        public APMad(List<string> sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            return sentences.Count > 0 ? sentences.Dequeue() : "";
+        }
+
+        public override bool Completed() => sentences.Count == 0;
     }
 
-    // O(1) dequeue
-    public override string Action(string ear, string skin, string eye)
+    public class APShy : AlgPart
     {
-        return _sentences.Count > 0 ? _sentences.Dequeue() : "";
+        private readonly Queue<string> sentences;
+
+        public APShy(params string[] sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
+
+        public APShy(List<string> sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            return sentences.Count > 0 ? sentences.Dequeue() : "";
+        }
+
+        public override bool Completed() => sentences.Count == 0;
     }
 
-    // O(1) completed check
-    public override bool Completed()
+    public class APHappy : AlgPart
     {
-        return _sentences.Count == 0;
-    }
-}
+        private readonly Queue<string> sentences;
 
-public class APHappy : AlgPart
-{
-    private readonly Queue<string> _sentences;
+        public APHappy(params string[] sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
 
-    // Constructor for params
-    public APHappy(params string[] sentences)
-    {
-        _sentences = new Queue<string>(sentences);
-    }
+        public APHappy(List<string> sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
 
-    // Constructor for List
-    public APHappy(List<string> list1)
-    {
-        _sentences = new Queue<string>(list1);
-    }
+        public override string Action(string ear, string skin, string eye)
+        {
+            return sentences.Count > 0 ? sentences.Dequeue() : "";
+        }
 
-    // O(1) dequeue
-    public override string Action(string ear, string skin, string eye)
-    {
-        return _sentences.Count > 0 ? _sentences.Dequeue() : "";
+        public override bool Completed() => sentences.Count == 0;
     }
 
-    // O(1) completed check
-    public override bool Completed()
+    public class APSad : AlgPart
     {
-        return _sentences.Count == 0;
-    }
-}
+        private readonly Queue<string> sentences;
 
-public class APSad : AlgPart
-{
-    private readonly Queue<string> _sentences;
+        public APSad(params string[] sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
 
-    // Constructor for params
-    public APSad(params string[] sentences)
-    {
-        _sentences = new Queue<string>(sentences);
-    }
+        public APSad(List<string> sentences)
+        {
+            this.sentences = new Queue<string>(sentences);
+        }
 
-    // Constructor for List
-    public APSad(List<string> list1)
-    {
-        _sentences = new Queue<string>(list1);
-    }
+        public override string Action(string ear, string skin, string eye)
+        {
+            return sentences.Count > 0 ? sentences.Dequeue() : "";
+        }
 
-    // O(1) dequeue
-    public override string Action(string ear, string skin, string eye)
-    {
-        return _sentences.Count > 0 ? _sentences.Dequeue() : "";
+        public override bool Completed() => sentences.Count == 0;
     }
 
-    // O(1) completed check
-    public override bool Completed()
+    public class APSkillRemover : AlgPart
     {
-        return _sentences.Count == 0;
+        private readonly Brain brain;
+        private readonly Skill skillToRemove;
+        private bool done = false;
+
+        public APSkillRemover(Brain brain, Skill skillToRemove)
+        {
+            this.brain = brain;
+            this.skillToRemove = skillToRemove;
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            brain.RemoveSkill(skillToRemove);
+            done = true;
+            return "";
+        }
+
+        public override bool Completed() => done;
+    }
+
+    public class APSkillAdder : AlgPart
+    {
+        private readonly Brain brain;
+        private readonly Skill skillToAdd;
+        private bool done = false;
+
+        public APSkillAdder(Brain brain, Skill skillToAdd)
+        {
+            this.brain = brain;
+            this.skillToAdd = skillToAdd;
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            brain.AddSkill(skillToAdd);
+            done = true;
+            return "";
+        }
+
+        public override bool Completed() => done;
+    }
+
+    public class APSkillSwapper : AlgPart
+    {
+        private readonly Brain brain;
+        private readonly Skill skillToRemove;
+        private readonly Skill skillToAdd;
+        private bool done = false;
+
+        public APSkillSwapper(Brain brain, Skill skillToRemove, Skill skillToAdd)
+        {
+            this.brain = brain;
+            this.skillToRemove = skillToRemove;
+            this.skillToAdd = skillToAdd;
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            brain.RemoveSkill(skillToRemove);
+            brain.AddSkill(skillToAdd);
+            done = true;
+            return "";
+        }
+
+        public override bool Completed() => done;
+    }
+
+    public class APSkillsAdder : AlgPart
+    {
+        private readonly Brain brain;
+        private readonly Skill[] skillsToAdd;
+        private bool done = false;
+
+        public APSkillsAdder(Brain brain, params Skill[] skillsToAdd)
+        {
+            this.brain = brain;
+            this.skillsToAdd = skillsToAdd;
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            foreach (var skill in skillsToAdd)
+                brain.AddSkill(skill);
+            done = true;
+            return "";
+        }
+
+        public override bool Completed() => done;
+    }
+
+    public class APSkillsRemover : AlgPart
+    {
+        private readonly Brain brain;
+        private readonly Skill[] skillsToRemove;
+        private bool done = false;
+
+        public APSkillsRemover(Brain brain, params Skill[] skillsToRemove)
+        {
+            this.brain = brain;
+            this.skillsToRemove = skillsToRemove;
+        }
+
+        public override string Action(string ear, string skin, string eye)
+        {
+            foreach (var skill in skillsToRemove)
+                brain.RemoveSkill(skill);
+            done = true;
+            return "";
+        }
+
+        public override bool Completed() => done;
     }
 }
