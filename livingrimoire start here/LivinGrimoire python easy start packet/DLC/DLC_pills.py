b885931532bee7f5bae5006ed3da11cbe090be9f
yotamarker <yotamarker@protonmail.ch>
2026-08-22 07:53:04 +0300

    pill dlc touch up 220826

--- livingrimoire start here/LivinGrimoire python easy start packet/DLC/DLC_pills.py
+++ livingrimoire start here/LivinGrimoire python easy start packet/DLC/DLC_pills.py
@@ -1,8 +1,6 @@
 from DLC.pills import PillAccelo
 from LivinGrimoirePacket.LivinGrimoire import Brain
 
-import __main__  # noqa
-
 def add_DLC_skills(brain: Brain):
     brain.add_skill(PillAccelo(brain))
     pass
